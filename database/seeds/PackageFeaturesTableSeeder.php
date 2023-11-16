600b560b240f30425090bbc3800e6793233e55be
Shoaib Ahmad <itianzinfo@gmail.com>
2023-11-16 11:49:16 +0500

    fix seeder issue

--- database/seeds/PackageFeaturesTableSeeder.php
+++ database/seeds/PackageFeaturesTableSeeder.php
@@ -11,11 +11,11 @@ class PackageFeaturesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('package_features')->insert([
-            ['package_id' => 1, 'feature_id' => 1],
-            ['package_id' => 1, 'feature_id' => 2],
-            ['package_id' => 2, 'feature_id' => 2],
-            // Add more assignments as needed
-        ]);
+        // DB::table('package_features')->insert([
+        //     ['package_id' => 1, 'feature_id' => 1],
+        //     ['package_id' => 1, 'feature_id' => 2],
+        //     ['package_id' => 2, 'feature_id' => 2],
+        //     // Add more assignments as needed
+        // ]);
     }
 }
